fa5838169fe6179adc1a4a158c4f44b07faaf36a
The Law <The Law@DESKTOP-RVI4UJI>
2019-06-23 18:50:41 -0400

    package controladorr (se modifico el nombre antes llamado CONEXION) "GESTIONESTUDIANTIL"

---
+++ src/controlador/Conexion.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlador;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author The Law
+ */
+public class Conexion {
+    
+    Connection conexion = null;
+    Statement sentencia = null;
+    
+    public String rutaDB; 
+    
+    public Conexion(){
+        rutaDB = "C:\\Users\\The Law\\Documents\\NetBeansProjects\\GestionEstudiantil\\Gestion_Estudiantil.sqlite";
+    }
+    
+    public void Conectar(){
+        
+        try{
+            Class.forName("org.sqlite.JDBC");
+        
+        }catch(Exception e){
+            JOptionPane.showMessageDialog(null, e.getMessage());
+        }
+            
+            try{
+                conexion = DriverManager.getConnection("jdbc:sqlite:"+rutaDB);
+                sentencia = conexion.createStatement();
+                JOptionPane.showMessageDialog(null, "Conectado");
+            }catch (Exception e){
+                JOptionPane.showMessageDialog(null, e.getMessage());
+            }
+    }
+    
+}
--- src/controlador/Ctrl_login.java
+++ src/controlador/Ctrl_login.java
@@ -5,10 +5,40 @@
  */
 package controlador;
 
+import controlador.Conexion;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author The Law
  */
-public class Ctrl_login {
+public class Ctrl_login extends Conexion{
+    
+    public Ctrl_login(){
+        
+    }
+    
+    public boolean insertar(String sql){
+        boolean valor = true;
+        Conectar();
+        try {
+            sentencia.executeUpdate(sql);
+        } catch (SQLException e) {
+                valor = false;
+                JOptionPane.showMessageDialog(null, e.getMessage());
+            }      
+        finally{  
+            try{    
+                 sentencia.close();  
+                 conexion.close();  
+             }catch (Exception e){                 
+                 e.printStackTrace();  
+             }  
+        }
+        return valor;
+    }
     
 }
