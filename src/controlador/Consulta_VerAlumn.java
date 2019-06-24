bc7ddc9b67fc508c41cf0d911229526d7e3df2b7
joaquinvalenzuela <joaquin.valenzuela.jv2@gmail.com>
2019-06-23 23:06:25 -0400

    Creacion del objeto alumno "GESTIONESTUDIANTIL"

---
+++ src/controlador/Consulta_VerAlumn.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlador;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+/**
+ *
+ * @author joako
+ */
+public class Consulta_VerAlumn extends Conexion {
+
+    public void VerAlumn(JTable tbtVerAlumn, String grado) {
+        DefaultTableModel modelo = new DefaultTableModel();
+        modelo.addColumn("Nombre"); //agrega columnas
+        modelo.addColumn("Apellido");
+        modelo.addColumn("Rut");
+        modelo.addColumn("Estado Matricula");
+        String[] datos = new String[4]; // arreglo para recorrer la tabla
+        tbtVerAlumn.setModel(modelo);
+        ResultSet resultado = null;
+
+        Conectar();
+        try {
+            String sql = "SELECT nombre_alumno , apellido_alumno ,rut_alumno ,estado_matricula"
+                    + " FROM Alumno "
+                    + " JOIN Matricula "
+                    + " ON Alumno.rut_alumno = Matricula.rut_alumno"
+                    + " JOIN Detalle_Curso "
+                    + " ON Alumno.rut_alumno = Detalle_Curso.rut_alumno7"
+                    + " WHERE grado = '" + grado + "'";
+
+            resultado = sentencia.executeQuery(sql);
+
+            while (resultado.next()) {
+                datos[0] = resultado.getString("nombre_alumno");
+                datos[1] = resultado.getString("apellido_alumno");
+                datos[2] = resultado.getString("rut_alumno");
+                datos[3] = resultado.getString("estado_matricula");
+
+                modelo.addColumn(datos);
+            }
+            tbtVerAlumn.setModel(modelo);
+            sentencia.close();
+            conexion.close();
+
+        } catch (SQLException e) {
+                JOptionPane.showMessageDialog(null, "Error: " + e.getMessage());
+        }
+
+    }
+
+}
