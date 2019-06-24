fb94016c270c65a45c6817395ea7930a301b1ffb
kimbe <kimbe@DESKTOP-TRC4ODF>
2019-06-23 20:45:53 -0400

    creacion de clase especialidad "GESTIONESTUDIANTIL"

---
+++ src/objeto/Especialidad.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package objeto;
+
+/**
+ *
+ * @author kimbe
+ */
+public class Especialidad {
+    
+    private String nombre_Especialidad;
+
+    public Especialidad() {
+    }
+
+    public Especialidad(String nombre_Especialidad) {
+        this.nombre_Especialidad = nombre_Especialidad;
+    }
+
+    public String getNombre_Especialidad() {
+        return nombre_Especialidad;
+    }
+
+    public void setNombre_Especialidad(String nombre_Especialidad) {
+        this.nombre_Especialidad = nombre_Especialidad;
+    }
+    
+    
+    
+    
+}
